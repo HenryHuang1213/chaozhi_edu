76433a015ee814e98bac60e9b3e82a3be49ca674
He Huang <henryhuang1213@gmail.com>
2023-06-30 22:50:02 +1000

    update 4.2

---
+++ ocr_pro.py
@@ -0,0 +1,15 @@
+import pytesseract
+
+from PIL import Image
+
+def ocr_image(image_path):
+    image = Image.open(image_path)
+    text = pytesseract.image_to_string(image, lang='chi_sim')
+    return text
+
+text = ocr_image('data\\20230623133051.jpg')
+print(text)
+
+
+
+
